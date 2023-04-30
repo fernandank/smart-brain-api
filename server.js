40aeec24380ab75aa57180bd01359c32ae55d956
fernandank <fenakandakari@gmail.com>
2023-04-30 18:55:39 -0300

    uploading my version

--- server.js
+++ server.js
@@ -1,37 +1,44 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
+const cors= require('cors');
 const knex = require('knex');
-
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
 
 const db = knex({
-  // connect to your own database here:
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    connectionString : 'postgres://backend_of_the_face_recognition_app_user:0piGRzDJ9YuWTHC6XyFwlscZkL87AJka@dpg-ch76j1g2qv26p1b9rtbg-a/backend_of_the_face_recognition_app',
+    host: 'dpg-ch76j1g2qv26p1b9rtbg-a.ohio-postgres.render.com',
+    port: 5432,
+    user: 'backend_of_the_face_recognition_app_user',
+    password: '0piGRzDJ9YuWTHC6XyFwlscZkL87AJka',
+    database: 'backend_of_the_face_recognition_app'
   }
 });
 
+
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+//Sigin route
+app.post('/signin', (req,res) => { signin.handleSignin(req, res, db, bcrypt)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+//Register route
+app.post('/register',(req,res) => { register.handleRegister(req, res, db, bcrypt)})
+
+//User ID
+app.get('/profile/:id', (req,res) => { profile.handleProfileGet(req, res, db)})
+
+//counter of images
+app.put('/image', (req, res) => { image.handleImage(req,res,db)})
+app.post('/imageUrl', (req,res) => { image.handleApiCall(req,res)})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('app is ok on port ${process.env.PORT}');
 })
