20f5d65c9152c55df51cb7f23ae495c3187958ac
Fernanda Hirata <122220584+fernandank@users.noreply.github.com>
2023-06-09 17:39:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : 'process.env.DATABASE_URL',
-    host: 'process.env.DATABASE_HOST',
+    connectionString :  process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST,
     port: 5432,
-    user: 'process.env.DATABASE_USER',
-    password: 'process.env.DATABASE_PW',
-    database: 'process.env.DATABASE_DB'
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
   }
 });
 
