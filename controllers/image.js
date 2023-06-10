07dc3fccb839ce6f74992cf963a550bb8082db0c
Fernanda Hirata <122220584+fernandank@users.noreply.github.com>
2023-06-10 10:41:30 -0300

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,23 +1,44 @@
-const Clarifai = require('clarifai');
-
-
-const app = new Clarifai.App({
- apiKey: '7c1e9f21a94b46e28f1efae16a6d2f19' 
-});
-
 
 const handleApiCall = (req, res) => {
-	app.models.predict({
-		id: 'face-detection',
-		name: 'face-detection',
-		version: '6dc7e46bc9124c5c8824be4822abe105',
-		type: 'visual-detector',
-	      }, req.body.input)
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
+	
+	const PAT = '075c027ce7b34fcd80133a2022398523';
+	const USER_ID = 'fernandank';
+	const APP_ID = 'test_1';
+	// Change these to whatever model and image URL you want to use
+	const MODEL_ID = 'face-detection';
+	const IMAGE_URL = 'imageUrl';
+
+	const raw = ({ 
+		  {
+		user_app_id: {
+		    "user_id": USER_ID,
+		    "app_id": APP_ID
+		},
+		model_id: MODEL_ID,
+
+		inputs: [
+		    { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
+		]
+	    },
+		    
+ 	   axios.post("https://api.clarifai.com/v2/models/face-detection/versions/45fb9a671625463fa646c3523a3087d5/outputs", 
+                raw,
+                { 
+                    headers: {
+                        'Accept': 'application/json',
+                        'Authorization': 'Key ' + PAT
+                    }
+                }
+            )
+        .then(response => {
+            res.json(response.data)
+
+        })
+        .catch(err => res.status(400).json('unable to work with api'))
 }
+
+
+		    
 
 const handleImage = (req,res, db) => {
 	const { id } = req.body;
