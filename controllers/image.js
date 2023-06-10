8dd037b2a06c58d22d4df08cbc82018372d23fa1
Fernanda Hirata <122220584+fernandank@users.noreply.github.com>
2023-06-10 11:01:06 -0300

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,46 +1,16 @@
-const axios = require('axios')
+const app = new Clarifai.App({
+ apiKey: '7c1e9f21a94b46e28f1efae16a6d2f19' 
+});
+
 
 const handleApiCall = (req, res) => {
-	
-	const PAT = '075c027ce7b34fcd80133a2022398523';
-	const USER_ID = 'fernandank';
-	const APP_ID = 'test_1';
-	// Change these to whatever model and image URL you want to use
-	const MODEL_ID = 'face-detection';
-	const IMAGE_URL = 'imageUrl';
-	const raw = {
-		"user_app_id": {
-		    "user_id": 'nessabyte',
-		    "app_id": 'my-first-application'
-		},
-		"inputs": [
-		    {
-			"data": {
-			    "image": {
-				"url": IMAGE_URL
-			    }
-			}
-		    }
-		]
-	    };
-   
- 	   axios.post("https://api.clarifai.com/v2/models/face-detection/versions/45fb9a671625463fa646c3523a3087d5/outputs", 
-                raw,
-                { 
-                    headers: {
-                        'Accept': 'application/json',
-                        'Authorization': 'Key ' + PAT
-                    }
-                }
-            )
-        .then(response => {
-            res.json(response.data)
 
-        })
-        .catch(err => res.status(400).json('unable to work with api'))
+  app.models.predict('face-detection', req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
 }
-
-
 		    
 
 const handleImage = (req,res, db) => {
